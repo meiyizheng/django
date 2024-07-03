f064d174f31ac65a68aefbd5a2784e1115605979
Meiyi Zheng <meiyizheng@yahoo.com>
2024-07-02 21:37:17 -0700

    try to install django debugger tool

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #"debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    #"debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "mysite.urls"
@@ -94,7 +96,11 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
-
+#INTERNAL_IPS = [
+    # ...
+#    "127.0.0.1",
+    # ...
+#]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
